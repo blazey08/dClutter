201bf9de7a8cce9236ced16b1fec2318704dd462
blazey08 <lauyuhui18@yahoo.com.sg>
2024-06-17 16:55:01 +0800

    link frontend to backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http://localhost:5000",
+  server: {
+    proxy: {
+      "/": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
